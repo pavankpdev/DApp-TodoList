8fc9c9f0c3226964c01290cdb049575cd1ee848c
pavanleton <pavankumarkpkp@gmail.com>
2021-10-06 09:37:29 +0530

    add createTask function

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,23 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    constructor() public {
+        createTask("Testing...!");
+    }
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    function createTask(string memory _content) public{
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+
 }
